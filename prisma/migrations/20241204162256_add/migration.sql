562fadf666a28f8b4c991d2f3175f979a81a94e0
@dev.arthur <contato.arthurreis222@gmail.com>
2024-12-04 13:23:09 -0300

    chore: add user model

---
+++ prisma/migrations/20241204162256_add/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('OWNER', 'EDITOR', 'AUTHOR');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'AUTHOR',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
