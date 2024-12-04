f0677a0954a36fb314fffd7502d025796e956c95
@dev.arthur <contato.arthurreis222@gmail.com>
2024-12-04 14:12:56 -0300

    feat: create auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,26 @@
+import { Logo } from '@/components/ui/logo'
 import { PropsWithChildren } from 'react'
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
-  return <div>AuthLayout{children}</div>
+  return (
+    <div className="h-screen grid lg:grid-cols-[1fr_800px]">
+      <div className="hidden lg:flex bg-custom-gradient items-center justify-center flex-col gap-6 text-white font-medium text-center">
+        <div>
+          <div className="text-7xl font-bold">Bem-vindo ao</div>
+          <div className="text-7xl font-bold">Blogora!👋</div>
+        </div>
+
+        <div className="text-xl text-left">Dê voz aos seus pensamentos.</div>
+      </div>
+
+      <div className="flex items-center justify-center flex-col">
+        <div className="mb-20">
+          <Logo />
+        </div>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default AuthLayout
---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Logo = () => {
+  return (
+    <div className="flex items-center justify-center text-xl font-semibold">
+      blogora <span className="text-4xl text-blue-500">.</span>
+    </div>
+  )
+}
